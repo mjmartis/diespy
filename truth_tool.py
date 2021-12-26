df57f67ec8286d4add82b7df019f0096f65d4f93
Michael Martis <michaeljmartis@gmail.com>
2021-12-26 21:47:31 +0800

    Added image loading with scroll bars

--- truth_tool.py
+++ truth_tool.py
@@ -5,8 +5,12 @@
 #  image_file_name,p1x,p1y,p2x,p2y,die_type,die_value
 
 import sys
+import tkinter as tk
+from PIL import ImageTk, Image
 
 FILE_LABEL_LEN = 9
+WIN_DIMS = '1200x800'
+CANVAS_DIM = 600
 
 def parse_labels(labels_f):
   ls = []
@@ -32,7 +36,32 @@ def main():
       (file_label_idxs if len(l) == FILE_LABEL_LEN else label_idxs).append(i)
     file_label_idx = None if not file_label_idxs else file_label_idxs[0]
 
-    print(label_idxs, file_label_idxs)
+  root = tk.Tk()
+  root.geometry(WIN_DIMS)
+
+  # Add a frame that can display our source image with scroll bars.
+  frame = tk.Frame(root, width=CANVAS_DIM, height=CANVAS_DIM)
+  frame.pack(anchor=tk.NW, fill=None, expand=False)
+  frame.pack_propagate(0)
+
+  # Add a canvas that actually displays our source image.
+  img = ImageTk.PhotoImage(Image.open(image_fn))
+  canvas = tk.Canvas(frame, scrollregion=(0, 0, img.width(), img.height()))
+
+  # Add scroll bars.
+  hbar = tk.Scrollbar(frame, orient=tk.HORIZONTAL)
+  hbar.pack(side=tk.BOTTOM, fill=tk.X)
+  hbar.config(command=canvas.xview)
+  vbar = tk.Scrollbar(frame, orient=tk.VERTICAL)
+  vbar.pack(side=tk.RIGHT, fill=tk.Y)
+  vbar.config(command=canvas.yview)
+  canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+
+  # Make our canvas fill the whole frame.
+  canvas.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+  canvas.create_image(0, 0, anchor=tk.NW, image=img)
+
+  root.mainloop()
 
 if __name__ == '__main__':
   main()
